43159bf5ea73124772a26ac51c9ae9542c09b1ad
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-16 08:48:16 +0300

    Added 'AreasAdmin' area to the project Created 'UserViewModel' class inside the 'AreasAdmin' area Implemented 'UserList' in HomeController and displayed the list of users

---
+++ NetCoreIdentityApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreIdentityApp.Web.Areas.Admin.Models;
+using NetCoreIdentityApp.Web.Models;
+
+namespace NetCoreIdentityApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await _userManager.Users.ToListAsync();
+            var userViewModelList = userList.Select(u => new UserViewModel()
+            {
+                Id = u.Id,
+                Email = u.Email,
+                Name = u.UserName
+            }).ToList();
+            return View(userViewModelList);
+        }
+    }
+}
