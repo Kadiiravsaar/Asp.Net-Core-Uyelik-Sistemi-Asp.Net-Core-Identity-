9cd90c613ef1320a83b1ed6cd045621cd954b34b
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-17 05:23:05 +0300

    PasswordValidator and UserValidator added

---
+++ NetCoreIdentityApp.Web/CustomValidation/UserValidator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NetCoreIdentityApp.Web.Models;
+
+namespace NetCoreIdentityApp.Web.CustomValidation
+{
+    public class UserValidator : IUserValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            var errors = new List<IdentityError>();
+            var isDigit = int.TryParse(user.UserName![0].ToString(), out _);
+
+            if (isDigit)
+            {
+                errors.Add(new() { Code = "UserNameContainFirstLetterDigit", Description = "Kullanıcı adının ilk karekteri sayısal bir karakter içeremez" });
+            }
+
+            if (errors.Any())
+            {
+                return Task.FromResult(IdentityResult.Failed(errors.ToArray()));
+            }
+
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
