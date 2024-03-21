b5f7baad23ee279d09c5533941c79f1665b668be
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-21 06:35:04 +0300

    Cookie Options

--- NetCoreIdentityApp.Web/Program.cs
+++ NetCoreIdentityApp.Web/Program.cs
@@ -14,9 +14,28 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
 });
 
-
-
 builder.Services.AddIdentityWithExt();
+
+
+builder.Services.ConfigureApplicationCookie(opt =>
+{ // Identity'nin çerez tabanlý kimlik doðrulama hizmetini yapýlandýrýr.
+
+    var cookieBuilder = new CookieBuilder(); // CookieBuilder sýnýfý, kimlik doðrulama çerezinin özelliklerini ayarlamak için yöntemler saðlar.
+
+    cookieBuilder.Name = "IdentityLearn";
+    // Bu satýr, çerezin adýný "IdentityLearn" olarak ayarlar. Varsayýlan olarak, çerez adý ".AspNetCore.Identity.Application".Daha iyi tanýmlama için burada özelleþtirebilirsiniz.
+
+    opt.LoginPath = new PathString("/Home/SignIn");
+    //Kullanýcýlar korunan bir kaynaða eriþmeye çalýþýrken ve kimlik doðrulamasý yapmamýþsa, oturum açmak için bu yola yönlendirilirler.
+
+    opt.Cookie = cookieBuilder;
+    opt.ExpireTimeSpan = TimeSpan.FromDays(30);
+    //Bu satýr, kimlik doðrulama çerezinin son kullanma süresini ayarlar.
+
+    opt.SlidingExpiration = true;
+    //Bu, kullanýcýnýn uygulamada aktif olarak kullandýðý sürece oturum açmýþ kalmasýný saðlar, bu da ExpireTimeSpan'dan daha uzun olsa bile.
+
+});
 
 
 var app = builder.Build();
