2bef287e756700abad605ddc620a2015d38c404d
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-22 07:38:01 +0300

    Member/Index Added

---
+++ NetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreIdentityApp.Web.Models;
+
+namespace NetCoreIdentityApp.Web.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public MemberController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
