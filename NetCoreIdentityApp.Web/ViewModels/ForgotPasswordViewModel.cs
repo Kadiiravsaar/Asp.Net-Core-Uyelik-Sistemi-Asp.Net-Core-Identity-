db28c49920f587b84006615f9e8f8afc991d9236
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-23 06:40:38 +0300

    ForgotPassword - Created ForgotPasswordViewModel class. - Created ForgotPassword.cshtml page for the 'forgot password' section.

---
+++ NetCoreIdentityApp.Web/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreIdentityApp.Web.ViewModels
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required(ErrorMessage = "Email alanı boş bırakılamaz.")]
+        [EmailAddress(ErrorMessage = "Email formatı yanlıştır.")]
+        [Display(Name = "Email ")]
+        public string Email { get; set; } = null!;
+    }
+}
