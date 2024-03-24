97a3b3c6834b902379080eefd9384cffa3689482
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-24 08:25:59 +0300

    E-mail sending process: Added IEmailService interface and SendResetPasswordEmail signature Created EmailService class and implemented IEmailService methods Configured smtp and MailMessage settings in EmailService

---
+++ NetCoreIdentityApp.Web/Services/EmailService.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.Extensions.Options;
+using NetCoreIdentityApp.Web.OptionsModels;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+
+namespace NetCoreIdentityApp.Web.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendResetPasswordEmail(string resetPasswordEmailLink, string toEmail)
+        {    
+            var smtpClient = new SmtpClient();
+            smtpClient.Host = _emailSettings.Host;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Port = 587;
+            smtpClient.Credentials = new NetworkCredential(_emailSettings.Email, _emailSettings.Password);
+            smtpClient.EnableSsl =true;
+
+
+
+            var mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(toEmail);
+            mailMessage.To.Add(toEmail);
+            mailMessage.Subject = "Localhost şifre sıfırlama linki";
+            mailMessage.Body = @$"
+                  <h4>Şifrenizi yenilemek için aşağıdaki linkte tıklayınız.</h4>
+                  <p><a href='{resetPasswordEmailLink}'>şifre yenileme link</a></p>";
+
+            mailMessage.IsBodyHtml = true;
+
+
+            await smtpClient.SendMailAsync(mailMessage);
+
+        }
+    }
+}
---
+++ NetCoreIdentityApp.Web/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace NetCoreIdentityApp.Web.Services
+{
+    public interface IEmailService
+    {
+        Task SendResetPasswordEmail(string resetPasswordEmailLink, string toEmail);
+    }
+}
