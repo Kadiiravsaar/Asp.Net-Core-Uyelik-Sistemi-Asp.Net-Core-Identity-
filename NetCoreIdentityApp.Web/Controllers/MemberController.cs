0a8694348657eec00f53b7e10369a57f0646b217
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-25 08:48:43 +0300

    Designing the Member Page Added UserManager as DI to MemberController and created login page for users Created UserViewModel for users

--- NetCoreIdentityApp.Web/Controllers/MemberController.cs
+++ NetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NetCoreIdentityApp.Web.Models;
+using NetCoreIdentityApp.Web.ViewModels;
 
 namespace NetCoreIdentityApp.Web.Controllers
 {
@@ -9,15 +10,25 @@ namespace NetCoreIdentityApp.Web.Controllers
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public MemberController(SignInManager<AppUser> signInManager)
+        public MemberController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name!);
+
+            var userViewModel = new UserViewModel
+            {
+                Email = currentUser!.Email,
+                UserName = currentUser!.UserName,
+                PhoneNumber = currentUser!.PhoneNumber
+            };
+            return View(userViewModel);
         }
         public async Task<IActionResult> Logout()
         {
---
+++ NetCoreIdentityApp.Web/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreIdentityApp.Web.ViewModels
+{
+    public class UserViewModel
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
