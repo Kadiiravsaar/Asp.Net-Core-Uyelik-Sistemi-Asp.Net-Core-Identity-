d18014ddd8f46de90d654201cb7aa27037ae317d
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-27 07:55:39 +0300

    Added new properties to Appuser class: City, Picture, BirthDate, Gender

--- NetCoreIdentityApp.Web/Models/AppUser.cs
+++ NetCoreIdentityApp.Web/Models/AppUser.cs
@@ -4,5 +4,9 @@ namespace NetCoreIdentityApp.Web.Models
 {
     public class AppUser : IdentityUser
     {
+        public string? City { get; set; }
+        public string? Picture { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public byte? Gender { get; set; }
     }
 }
