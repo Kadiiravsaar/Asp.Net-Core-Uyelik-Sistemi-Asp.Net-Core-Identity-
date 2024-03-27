dbac6c42a1d0f67bffe45e106d2e1cb13db919a9
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-27 07:55:57 +0300

    Migration Update

--- NetCoreIdentityApp.Web/Migrations/20240326054045_update_app_user.Designer.cs
+++ NetCoreIdentityApp.Web/Migrations/20240326054045_update_app_user.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetCoreIdentityApp.Web.Models;
 
@@ -11,9 +12,11 @@ using NetCoreIdentityApp.Web.Models;
 namespace NetCoreIdentityApp.Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240326054045_update_app_user")]
+    partial class update_app_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
