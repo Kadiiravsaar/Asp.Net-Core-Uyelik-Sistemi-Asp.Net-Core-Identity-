1302e7889b14ce865d6ae2a1fedcc958d6aaa1aa
kadiiravsaar <kadiiravsar@gmail.com>
2024-04-02 05:57:17 +0300

    Role => Index Edited the page where roles are listed

---
+++ NetCoreIdentityApp.Web/Areas/Admin/Models/RoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCoreIdentityApp.Web.Areas.Admin.Models
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
