55b91c7e4edee1b5c12321a349e87f6f9d945f93
kadiiravsaar <kadiiravsar@gmail.com>
2024-04-22 12:21:57 +0300

    AuthorizationHandler Provided access to a page for a specific duration 1

---
+++ NetCoreIdentityApp.Web/Requirements/ExchangeExpireRequirement.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace NetCoreIdentityApp.Web.Requirements
+{
+    public class ExchangeExpireRequirement : IAuthorizationRequirement
+    {
+    }
+
+
+    public class ExchangeExpireRequirementHandler : AuthorizationHandler<ExchangeExpireRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ExchangeExpireRequirement requirement)
+        {
+            var hasExchangeExpireDate = context.User.HasClaim(x => x.Type == "ExchangeExpireDate");
+
+            if (!hasExchangeExpireDate)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var exchangeExpireDate = context.User.FindFirst("ExchangeExpireDate");
+
+            if (DateTime.Now > Convert.ToDateTime(exchangeExpireDate.Value) ) 
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            context.Succeed(requirement);
+
+            return Task.CompletedTask;
+
+
+
+        }
+    }
+}
